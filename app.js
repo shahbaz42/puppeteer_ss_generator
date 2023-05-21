015792c0d34baa6525cc6e78bbfef1020c0e4721
Shahbaz <shahbaz_ali@outlook.in>
2023-05-21 13:32:07 +0530

    basic implementation of puppetier for testing

--- app.js
+++ app.js
@@ -2,23 +2,37 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import morgan from 'morgan';
+import puppeteer from 'puppeteer';
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
 
+const browser = await puppeteer.launch();
+let count = 0;
+
 app.get("/health", (req, res) => {
     res.status(200).send("OK");
 });
 
-app.post("/getImage", (req, res) => {
+app.get("/getImage", async (req, res) => {
+    const page = await browser.newPage();
+    count = count + 1;
 
+    await page.goto('https://developer.chrome.com/');
 
-    res.status(200).send(req.body);
+    // Set screen size
+    await page.setViewport({ width: 1366, height: 768 });
+
+    await page.screenshot({ path: `screenshot_${count}.png` });
+
+    page.close();
+
+    res.status(200).send("OK");
 });
 
-app.listen(process.env.PORT || 8000 , () => {
+app.listen(process.env.PORT || 8000, () => {
     console.log("Server is up and running");
 });
 
